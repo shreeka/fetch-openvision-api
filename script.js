77b48cf1908c3ea2f25ae558ef815d6f854c88f4
shiri <shiri7293@gmail.com>
2021-09-06 14:47:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const input = document.getElementById('file_upload');
+const form = document.getElementById('e_form');
+const api_url = 'https://cors.bridged.cc/https://api.openvisionapi.com/api/v1/detection';
+const result= document.getElementById('result');
+
+
+form.addEventListener('submit', handleFormSubmit);
+
+async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    var formData = new FormData();
+    formData.append('model','yolov4');
+    formData.append('image', input.files[0]);
+
+    fetch(api_url, {
+        method: 'POST',
+        body: formData
+    }).then(
+        response => response.json()
+    ).then(data => {
+            for(const prediction of data.predictions){
+                const listItem = document.createElement('li');
+                listItem.textContent = `${prediction.label}:${prediction.score}`;
+                result.appendChild(listItem);
+            }
+        }
+
+    ).catch(
+        error => console.log(error)
+    );
+
+
+
+}
+
